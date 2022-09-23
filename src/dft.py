c751508ce4b74679c82ec91fe92075bdf16493d2
elvirazetterberg <elvira.zetterberg@gmail.com>
2022-09-23 13:35:43 +0200

    test dft

--- src/dft.py
+++ src/dft.py
@@ -2,6 +2,7 @@
 
 from typing import Iterable
 from tree import T
+from collections import deque
 
 
 def in_order(t: T | None) -> Iterable[int]:
@@ -11,4 +12,34 @@ def in_order(t: T | None) -> Iterable[int]:
     >>> list(in_order(tree))
     [1, 2, 3, 4, 5]
     """
-    return  # FIXME
+
+    # # recursive
+    # if t == None:
+    #     return # return something?
+    # else:
+    #     return [in_order(t.left), t.val, in_order(t.right)]
+    
+    
+    stack = deque() # make right size?
+    
+    # t2 = t.right
+
+    # while t != None:
+    #     stack.append(deque().append(t.left, t.val, t.right))
+    # while t2.right != None:
+    #     pass
+
+    res = []
+    # append values to res list until the stack is empty
+    while True:
+        while t != None:
+            stack.append(t)
+            t = t.left
+        if len(stack) > 0:
+            t = stack.pop()
+            res.append(t.val)
+            t = t.right
+        else:
+            break
+
+    return res
